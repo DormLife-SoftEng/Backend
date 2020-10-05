9a755af731d8d5017a86d52e47c43df44b5514e5
p4perf4ce <amrest.c@ku.th>
2020-10-05 16:30:28 +0700

    init auth, user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-import { DormModule } from './Dorm/dorm.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
+import { DormModule } from './Dorm/dorm.module';
 @Module({
   imports: [
   	MongooseModule.forRoot(
