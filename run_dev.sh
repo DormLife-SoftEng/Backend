aeec598a93d149e91a0e113b9ec64f30e8b0ebfc
p4perf4ce <amrest.c@ku.th>
2020-10-05 16:30:28 +0700

    init auth, user

--- run_dev.sh
+++ run_dev.sh
@@ -1,1 +1,2 @@
-docker-compose -f docker-compose.dev.yml up
+docker-compose -f docker-compose.dev.yml up --force-recreate
+
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { DormModule } from './Dorm/dorm.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -10,6 +13,8 @@ import { DormModule } from './Dorm/dorm.module';
     'mongodb://mongol:27017/DormLife',
   	),
     DormModule,
+	AuthModule,
+	UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
