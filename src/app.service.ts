e5f3a9a3e8c7e9c789b2d2da77c4ae4268ee3104
p4perf4ce <amrest.c@ku.th>
2020-10-10 18:44:26 +0700

    Prework

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getAlive(): {isalive: boolean} {
+    return {isalive: true}
   }
 }
