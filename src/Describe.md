768bf134a8e38e008481c725a2204cb993dfc09a
p4perf4ce <amrest.c@ku.th>
2020-10-10 18:44:26 +0700

    Prework

---
+++ src/Describe.md
@@ -0,0 +1,11 @@
+- main.ts 
+
+    main entrypoint.
+
+- app.module.ts
+
+    where we tell nest which "main" module we using.
+
+- app.servic.ts
+
+    where we provide service for appController
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('isalive')
+  getAlive(@Res() res: Response): Response{
+    return res.status(HttpStatus.OK).json(this.appService.getAlive());
   }
 }
