239a6dd9d4424814a886672da19e04583628ca0f
allah69 <thanawin.s@ku.th>
2020-10-12 13:09:11 +0700

    added Dorm schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DormModule } from './Dorm/dorm.module';
 
 @Module({
   imports: [
   	MongooseModule.forRoot(
   		'MONGO_URI',
-  	),
+    ),
+    DormModule
   ],
   controllers: [AppController],
   providers: [AppService],
