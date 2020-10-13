a6e9673cb7c5c3cf5030c86f5db20eb784976e4c
p4perf4ce <amrest.c@ku.th>
2020-10-13 10:52:33 +0700

    Ignoring dorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,13 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
-import { DormModule } from './Dorm/dorm.module';
+// import { DormModule } from './Dorm/dorm.module';
 @Module({
   imports: [
   	MongooseModule.forRoot(
     'mongodb://mongol:27017/DormLife',
   	),
-    DormModule,
+//    DormModule,
 	AuthModule,
 	UsersModule
   ],
