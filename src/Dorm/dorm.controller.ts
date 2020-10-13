69c54c744919db550241b4dd4f5dd6bbaca18ceb
allah69 <thanawin.s@ku.th>
2020-10-13 11:31:59 +0700

    create add room/util methods.

--- src/Dorm/dorm.controller.ts
+++ src/Dorm/dorm.controller.ts
@@ -5,18 +5,30 @@ import { DormService } from './dorm.service';
 export class DormController {
   constructor(private readonly DormService: DormService) {}
 
-  @Post()
-  async addDorm(@Body('name') dormName:string,@Body('contact') contact:{telephone:string}) {
-    const genID = await this.DormService.insert(
-      dormName,
-      contact
-    );
-    return {id: genID};
+  // @Post()
+  // async addDorm(@Body('name') dormName:string,@Body('contact') contact:{telephone:string}) {
+  //   const genID = await this.DormService.insertDorm(
+  //     dormName,
+  //     contact
+  //   );
+  //   return {id: genID};
+  // }
+
+  // @Get()
+  // async getAlldorm() {
+  //   const dorms = await this.DormService.getAll();
+  //   return dorms
+  // }
+
+  @Post('/room')
+  async addRoom(@Body('room') roomlist:any[]) {
+    const rooms = await this.DormService.addRoom(roomlist);
+    return rooms;
   }
 
-  @Get()
-  async getAlldorm() {
-    const dorms = await this.DormService.getAll();
-    return dorms
+  @Post('util')
+  async addUtil(@Body('util') util:any[]) {
+    const utils = await this.DormService.addUtility(util);
+    return utils;
   }
 }
--- src/Dorm/dorm.module.ts
+++ src/Dorm/dorm.module.ts
@@ -1,13 +1,17 @@
-import { Module } from "@nestjs/common";
-import { MongooseModule } from "@nestjs/mongoose";
-import { Mongoose } from "mongoose";
-import { DormController } from "./dorm.controller";
-import { DormService } from "./dorm.service";
-import { DormSchema } from "./dorm.model";
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Mongoose } from 'mongoose';
+import { DormController } from './dorm.controller';
+import { DormService } from './dorm.service';
+import { DormSchema, RoomSchema, utilSchema } from './dorm.model';
 
 @Module({
-    imports:[MongooseModule.forFeature([{name: 'Dorm', schema: DormSchema}])],
-    controllers:[DormController],
-    providers:[DormService]
+  imports: [
+    MongooseModule.forFeature([{ name: 'Dorm', schema: DormSchema }]),
+    MongooseModule.forFeature([{ name: 'Room', schema: RoomSchema }]),
+    MongooseModule.forFeature([{ name: 'Utility', schema: utilSchema }]),
+  ],
+  controllers: [DormController],
+  providers: [DormService],
 })
 export class DormModule {}
