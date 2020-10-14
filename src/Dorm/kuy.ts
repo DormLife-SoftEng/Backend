3ef381c187a61e07e108c11ab6b2d572155f5111
ChRatchapol <ratcahpol.c@ku.th>
2020-10-14 13:36:58 +0700

    try to fix /dorms

---
+++ src/Dorm/kuy.ts
@@ -0,0 +1,25 @@
+export interface propsSearch {
+    //dorm
+    dormName? : string,
+    price? : number,
+    distance? : number,
+    maxperson? : number,
+    rating? : number,
+    gender? : string,
+    //room
+    roomSize? : number,
+    roomType? : string,
+    //util
+    // in utility.type ?
+    convenienceStore? : boolean,
+    kitchen? : boolean,
+    laundry? : boolean,
+    parking? : boolean,
+    pet? : boolean,
+    internet? : boolean,
+    smoking? : boolean,
+    fitness? : boolean,
+    pool? : boolean,
+    cooking? : boolean,
+    restRoom? : boolean,
+}
