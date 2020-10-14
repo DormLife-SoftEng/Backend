c51e78b746ba9831ae759c421ef69e04296d51bb
p4perf4ce <amrest.c@ku.th>
2020-10-14 12:18:26 +0700

    express -> fastify

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from '@nestjs/common';
+import {
+		FastifyAdapter,
+		NestFastifyApplication
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+		 	AppModule,
+  			new FastifyAdapter({logger: true}));
+  app.setGlobalPrefix('api/v1');
   app.useGlobalPipes( new ValidationPipe() );
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`)
 }
 bootstrap();
