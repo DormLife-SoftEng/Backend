710b29c2a0c03b5ff8708678435f6d16f25e87c5
p4perf4ce <amrest.c@ku.th>
2020-10-14 17:57:38 +0700

    swagger

--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,37 @@ import {
 		NestFastifyApplication
 } from '@nestjs/platform-fastify';
 import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
-
+import * as FastifyFormBody from 'fastify-formbody';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
 		 	AppModule,
-  			new FastifyAdapter({logger: true}));
+  			new FastifyAdapter({logger: true}),
+			{bodyParser: false},
+ 			);
 
   app.setGlobalPrefix('api/v1');
+  app.register(FastifyFormBody as any);
   const options = new DocumentBuilder()
   	.setTitle('API Example')
 	.setDescription('DormLife API description')
 	.setVersion('1.0')
 	.setBasePath('api/v1')
+	.addServer('http://localhost:5000')
+	.addOAuth2(
+	  {
+		type: 'oauth2',
+		flows: {
+		  password: {
+		  	authorizationUrl: 'api/v1/oauth/verify-token',
+			tokenUrl: 'api/v1/oauth/sign-in',
+			refreshUrl: 'api/v1/oauth/refresh-token',
+			scopes: [],
+		  }
+		},
+		description: 'OpenAuth2',
+	  }
+	)
 	.build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/api', app, document);
