61f48c29f7323f78336fb717ed4ce07e78d1190d
p4perf4ce <amrest.c@ku.th>
2020-10-14 16:42:52 +0700

    reconfig swagger

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,23 @@ import {
 		FastifyAdapter,
 		NestFastifyApplication
 } from '@nestjs/platform-fastify';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
 		 	AppModule,
   			new FastifyAdapter({logger: true}));
+
   app.setGlobalPrefix('api/v1');
+  const options = new DocumentBuilder()
+  	.setTitle('API Example')
+	.setDescription('DormLife API description')
+	.setVersion('1.0')
+	.setBasePath('api/v1')
+	.build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/api', app, document);
   app.useGlobalPipes( new ValidationPipe() );
   await app.listen(3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`)
