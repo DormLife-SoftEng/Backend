4f07b00fa72ebce610d7d42c121fe1416835c40b
p4perf4ce <amrest.c@ku.th>
2020-10-14 16:42:52 +0700

    reconfig swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,6 @@
 import { IsNotEmpty, IsEmail, Matches, IsIn, IsBoolean, IsString, MinLength, MaxLength } from 'class-validator';
 import {Exclude, Expose} from 'class-transformer';
+import {ApiProperty} from '@nestjs/swagger';
 
 
 const sex: string[] = ['male', 'female'];
@@ -7,47 +8,56 @@ const allowedUsertype: string[] = ['general', 'owner'];
 
 @Exclude()
 export class CreateUserDto {
+	@ApiProperty()
 	@IsNotEmpty()
 	@IsString()
 	@Expose()
 	@Matches(/[a-zA-Z]+/)
 	readonly firstName: string;
 
+	@ApiProperty()
 	@IsNotEmpty()
 	@IsString()
 	@Expose()
 	@Matches(/[a-zA-Z]+/)
 	readonly lastName: string;
 
+	@ApiProperty()
 	@IsNotEmpty()
 	@IsString()
 	@Expose()
 	@Matches(/[0-9]+/)
 	readonly telephone: string;
 
+	@ApiProperty()
 	@IsNotEmpty()
 	@IsEmail()
 	@Expose()
 	readonly email: string;
 
+	@ApiProperty()
 	@IsNotEmpty()
 	@Expose()
 	@IsIn(["true", "false"])
 	email_verified: string;
 
+	@ApiProperty()
 	@IsIn(sex)
 	@Expose()
 	readonly sex: string;
 
+	@ApiProperty()
 	@IsNotEmpty()
 	@Expose()
 	password: string;
 
+	@ApiProperty()
 	@IsNotEmpty()
 	@Expose()
 	@IsIn(allowedUsertype)
 	userType: string;
 
+	@ApiProperty()
 	@IsNotEmpty()
 	@Expose()
 	@MinLength(13)
