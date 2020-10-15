7c20c2a66881a2bbb4d28b3c4690e5aac1eba113
p4perf4ce <amrest.c@ku.th>
2020-10-15 12:22:01 +0700

    added sign-out

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
-import {UserDocument} from 'src/users/schemas/users.schemas';
-import {JwtService} from '@nestjs/jwt';
-import {jwtPayload, jwtToken} from './auth.interface';
-import {UsersService} from '../users/users.service';
-import {jwtConstants} from './constants';
+import { UserDocument } from 'src/users/schemas/users.schemas';
+import { JwtService } from '@nestjs/jwt';
+import { jwtPayload, jwtToken } from './auth.interface';
+import { UsersService } from '../users/users.service';
+import { jwtConstants } from './constants';
 
 
 var bcrypt = require('bcryptjs');
@@ -18,6 +18,9 @@ export class AuthService {
 
 	async validateUser(username: string, password: string): Promise<any>{
 		const userDoc = await this.userServ.find(username);
+		if (!userDoc) {
+			return null;
+		}
 		const result = await bcrypt.compare(password, userDoc.hashedPassword);
 		if (result) {
 			return userDoc;
@@ -37,6 +40,7 @@ export class AuthService {
 			},
 			avatar: user.PictureProfile,
 			role: user.userType,
+			token_type: 'Bearer'
 		};
 		const access_options = {secret: jwtConstants.secret, expiresIn: '15m'};
 		const access_token: string = this.jwtServ.sign(payload, access_options);
@@ -55,14 +59,38 @@ export class AuthService {
 		return this.jwtServ.sign(payload, options);
 	}
 
-	async regerateTokens(refresh): Promise<jwtToken | undefined> {
-		// regenerate all tokens
+	async checkValidToken(refreshToken: string): Promise<any> {
+		const decodedPayload: any = this.jwtServ.decode(refreshToken)
+		const userDoc: UserDocument = await this.userServ.findById(decodedPayload.userId);
+		// revoke ?
+		if (userDoc.refreshToken != refreshToken) {
+			return null;
+		}
+		// return payload
+		return decodedPayload;
+	}
+
+	async invalidateToken(userId): Promise<any>{
+		const userDoc: UserDocument = await this.userServ.findById(userId);
+		if (!userDoc) {
+			return null;
+		}
+		userDoc.refreshToken = null;
+		userDoc.save();
+		return true;
+	}
+
+	async regenerateToken(refresh): Promise<jwtToken | undefined> {
+		// regenerate access token
 		const options = {secret: jwtConstants.refreshSecret};
 		if (await this.jwtServ.verify(refresh.refresh_token, options)) {
 			// If valid
-			const oldSingedPayload: any = this.jwtServ.decode(
-				refresh.refreshToken,
+			const oldSingedPayload: any = this.checkValidToken(
+				refresh.refresh_token
 			);
+			if (!oldSingedPayload) {
+				return null;
+			}
 			const newUnsignedPayload: jwtPayload = {
 				userId: oldSingedPayload.userId,
 				name: {
@@ -71,10 +99,11 @@ export class AuthService {
 				},
 				avatar: oldSingedPayload.avatar,
 				role: oldSingedPayload.role,
+				token_type: 'Bearer'
 			}
 			return {
 				access_token: this.jwtServ.sign(newUnsignedPayload),
-				refresh_token: await this.getRefreshToken(newUnsignedPayload),
+				refresh_token: oldSingedPayload,
 			}
 		}
 		else {
