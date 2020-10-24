fb5e0bb7f81e72598a3be2c087856b770c05a8a8
p4perf4ce <amrest.c@ku.th>
2020-10-24 16:25:04 +0700

    annotate schema

--- src/review/review.validation.ts
+++ src/review/review.validation.ts
@@ -1,33 +1,42 @@
 import { IsNotEmpty, IsArray, IsInt, IsString, ValidateIf, IsNumberString, IsNumber } from 'class-validator';
+import {ApiProperty} from '@nestjs/swagger';
+
 
 export class reviewCodeDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   reviewCode: string;
 }
 
 export class ReviewBodyDto {
   @IsNotEmpty()
+  @ApiProperty()
   dorm: any;
 
   @IsNotEmpty()
+  @ApiProperty()
   user: any;
 
   @IsNotEmpty()
   @IsInt()
+  @ApiProperty()
   star: number;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   comment: string;
 
   @IsNotEmpty()
   @IsArray()
+  @ApiProperty()
   image: [string];
 }
 
 export class ReviewParamDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   reviewId: string
 }
