68106606bb21262b0d07cd195f8144bcf62098cb
p4perf4ce <amrest.c@ku.th>
2020-10-25 18:35:29 +0700

    remove fastify

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from '@nestjs/common';
-import {
-		FastifyAdapter,
-		NestFastifyApplication
-} from '@nestjs/platform-fastify';
-import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
-import * as FastifyFormBody from 'fastify-formbody';
-const fmp = require('fastify-multipart');
+//import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
+  const app = await NestFactory.create(
 		 	AppModule,
-  			new FastifyAdapter({logger: true}),
-			{bodyParser: false},
  			);
 
   app.setGlobalPrefix('api/v1');
-  app.register(fmp);
-  app.register(FastifyFormBody as any);
   app.enableCors();
-  const options = new DocumentBuilder()
-  	.setTitle('API Example')
-	.setDescription('DormLife API description')
-	.setVersion('1.0')
-	.setBasePath('api/v1')
-	.addServer('http://localhost:5000')
-	.addOAuth2(
-	  {
-		type: 'oauth2',
-		flows: {
-		  password: {
-		  	authorizationUrl: 'api/v1/oauth/verify-token',
-			tokenUrl: 'api/v1/oauth/sign-in',
-			refreshUrl: 'api/v1/oauth/refresh-token',
-			scopes: [],
-		  }
-		},
-		description: 'OpenAuth2',
-	  }
-	)
-	.build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('/api', app, document);
+  //const options = new DocumentBuilder()
+      //.setTitle('API Example')
+	//.setDescription('DormLife API description')
+	//.setVersion('1.0')
+	//.setBasePath('api/v1')
+	//.addServer('http://localhost:5000')
+	//.addOAuth2(
+	  //{
+		//type: 'oauth2',
+		//flows: {
+		  //password: {
+			  //authorizationUrl: 'api/v1/oauth/verify-token',
+			//tokenUrl: 'api/v1/oauth/sign-in',
+			//refreshUrl: 'api/v1/oauth/refresh-token',
+			//scopes: [],
+		  //}
+		//},
+		//description: 'OpenAuth2',
+	  //}
+	//)
+	//.build();
+  //const document = SwaggerModule.createDocument(app, options);
+  //SwaggerModule.setup('/api', app, document);
   app.useGlobalPipes( new ValidationPipe() );
   await app.listen(3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`)
