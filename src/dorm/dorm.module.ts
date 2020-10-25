12a7bb3220f8b29eb06713441e82852a09a6ef4a
p4perf4ce <amrest.c@ku.th>
2020-10-25 17:24:35 +0000

    added deps

--- src/dorm/dorm.module.ts
+++ src/dorm/dorm.module.ts
@@ -3,15 +3,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { DormController } from './dorm.controller';
 import { DormService } from './dorm.service';
 import { DormSchema, RoomSchema, utilSchema } from './dorm.model';
+import { UsersModule } from 'src/users/users.module';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [
+    UsersModule,
     MongooseModule.forFeature([{ name: 'Room', schema: RoomSchema }]),
   	MongooseModule.forFeature([{ name: 'Dorm', schema: DormSchema }]),
     MongooseModule.forFeature([{ name: 'Utility', schema: utilSchema }]),
   ],
   controllers: [DormController],
-  providers: [DormService],
+  providers: [DormService, UsersService],
   exports: [DormService, MongooseModule.forFeature([{ name: 'Dorm', schema: DormSchema }]),],
 })
 export class DormModule {}
