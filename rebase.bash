18902d2b8eced00bc9dcf432b8f95a45ec08501b
p4perf4ce <amrest.c@ku.th>
2020-11-02 04:42:35 +0000

    Updating rebasing script

--- rebase.bash
+++ rebase.bash
@@ -1,3 +1,23 @@
-#!/bin/zsh
+#!/bin/bash
+if [ -z "$1"  ]; then
+    echo "Empty Argument. Aborting."
+    exit 1;
+fi
+if [ -z "$2" ]; then
+    echo "Empty Argument. Aborting."
+    exit 1;
+fi
+echo "Syncing $1 <-> $2 $3 ..."
 git fetch origin
-git rebase origin/master
+git checkout $1
+git pull
+sleep 0.5
+echo "Committing 2 ways rebase ..."
+echo "Syncing $1 -> $2 ..."
+git checkout $2
+git rebase $1
+sleep 0.5
+echo "Syncing $1 <- $2 ..."
+git checkout $1
+git rebase $2
+echo "Done"
