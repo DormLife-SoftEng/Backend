21e1df8d5eb38e90acd7b77bd7a1ad69f7dd6304
allah69 <thanawin.s@ku.th>
2020-11-23 02:31:51 +0700

    bug fix

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -28,10 +28,70 @@ export class ReviewController {
 
   @Get()
   @ApiQuery({ name: 'dormId', required: false })
-  @ApiQuery({ name: 'reviewCode', required: false })
+  // @ApiQuery({ name: 'reviewCode', required: false })
   @ApiQuery({ name: 'offset', required: false })
   @ApiQuery({ name: 'stop', required: false })
   async getReviewList(
+    @Query('dormId') dormId?: string,
+    // @Query('reviewCode') reviewCode?: string,
+    @Query('offset') offset?: string,
+    @Query('stop') stop?: string,
+  ) {
+    // if (
+    //   (reviewCode === undefined && dormId === undefined) ||
+    //   (reviewCode !== undefined && dormId !== undefined)
+    // ) {
+    //   throw new BadRequestException(
+    //     'Only one of reviewCode or dormId values ​​must be specified.',
+    //   );
+    // }
+
+    if (!(parseInt(offset) === parseFloat(offset) || offset === undefined)) {
+      throw new BadRequestException('offset must be integer.');
+    }
+
+    if (!(parseInt(stop) === parseFloat(stop) || stop === undefined)) {
+      throw new BadRequestException('stop must be integer.');
+    }
+
+    if (offset === undefined) {
+      offset = '0';
+    }
+
+    if (stop === undefined) {
+      stop = '50';
+    }
+    const reviews = await this.reviewService.getReviewList(
+      dormId,
+      offset,
+      stop,
+    );
+    return reviews;
+    // if (reviewCode === undefined) {
+    //   const reviews = await this.reviewService.getReviewList(
+    //     dormId,
+    //     offset,
+    //     stop,
+    //   );
+    //   return reviews;
+    // } else if (dormId === undefined) {
+    //   if (req.user.userId === undefined) {
+    //     throw new BadRequestException('userId must be defined.');
+    //   }
+    //   const review = await this.reviewService.getSingleReviewByReviewCode(
+    //     reviewCode,
+    //     req.user.userId,
+    //   );
+    //   return review;
+    // }
+  }
+
+  @Get('users')
+  @ApiQuery({ name: 'dormId', required: false })
+  @ApiQuery({ name: 'reviewCode', required: false })
+  @ApiQuery({ name: 'offset', required: false })
+  @ApiQuery({ name: 'stop', required: false })
+  async getUserReviewList(
     @Request() req,
     @Query('dormId') dormId?: string,
     @Query('reviewCode') reviewCode?: string,
